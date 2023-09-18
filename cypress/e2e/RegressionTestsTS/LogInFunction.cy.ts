fffe8425df5e7d6c62d20a4818a7a78087c0af99
Aleksander Avtushko <aleksandravtusko_gmail_com>
2023-09-18 23:34:49 +0300

    feat: updated login and password and added test

--- cypress/e2e/RegressionTestsTS/LogInFunction.cy.ts
+++ cypress/e2e/RegressionTestsTS/LogInFunction.cy.ts
@@ -1,4 +1,6 @@
-describe("Test log in ", () => {
+import { LoginData } from "../../fixtures/common";
+
+describe("Test log in functions", () => {
     beforeEach(() => {
         cy.fixture("appConsts").then((data) => {
             cy.visit(data.URL);
@@ -8,8 +10,25 @@ describe("Test log in ", () => {
     it("check: incorrect password", () => {
         cy.get('[class="sc-bRBYWo eOipuu"]').click();
         cy.get('[class="sc-VigVT sc-bmyXtO haGtID"]').type("ilya.stupen+1@imbuesystems.com");
-        cy.get('[class="sc-VigVT sc-bmyXtO duXQWC"]').type("wrongPassword");
+        cy.get('[class="sc-VigVT sc-bmyXtO duXQWC"]').type(LoginData.incorectPassword);
         cy.get('[class="sc-LKuAh sc-iBEsjs frrQCc"]').click();
         cy.get('[class="sc-cpmKsF dFrijd message"]').should("be.visible");
+    });
+
+    it("check: incorrect email", () => {
+        cy.get('[class="sc-bRBYWo eOipuu"]').click();
+        cy.get('[class="sc-VigVT sc-bmyXtO haGtID"]').type(LoginData.incorectEmail);
+        cy.get('[class="sc-VigVT sc-bmyXtO duXQWC"]').type("IlyaStupen_99");
+        cy.get('[class="sc-LKuAh sc-iBEsjs frrQCc"]').click();
+        cy.get('[class="sc-cpmKsF dFrijd message"]').should("be.visible");
+    });
+
+    it.only("check: incorrect email", () => {
+        cy.get('[class="sc-bRBYWo eOipuu"]').click();
+        cy.wait(5000);
+        cy.get('[class="sc-VigVT sc-bmyXtO haGtID"]').type(LoginData.correctEmail);
+        cy.get('[class="sc-VigVT sc-bmyXtO duXQWC"]').type(LoginData.correctPassword);
+        cy.get('[class="sc-LKuAh sc-iBEsjs frrQCc"]').click();
+        cy.get('[class="sc-fxMfqs enUwVe"]').should("be.visible").wait(5000);
     });
 });
