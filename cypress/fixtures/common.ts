f0b107bbc03eacdf04bb14051bc8a4891a751efc
Aleksander Avtushko <aleksandravtusko_gmail_com>
2023-09-18 23:32:19 +0300

    feat: added enum for login and password

---
+++ cypress/fixtures/common.ts
@@ -0,0 +1,6 @@
+export enum LoginData {
+    incorectEmail = "ilya.stupenWrongEmail@imbuesystems.com",
+    incorectPassword = "incorectPassword!@#$%123",
+    correctEmail = "ilya.stupen+1@imbuesystems.com",
+    correctPassword = "IlyaStupen_99",
+}
