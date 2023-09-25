10ce03d6d929f14a43baa2151686248bb3dda9ba
Aleksander Avtushko <aleksandravtusko_gmail_com>
2023-09-25 22:47:54 +0300

    feat: added enum data url for Login

--- cypress/fixtures/common.ts
+++ cypress/fixtures/common.ts
@@ -3,4 +3,5 @@ export enum LoginData {
     incorectPassword = "incorectPassword!@#$%123",
     correctEmail = "ilya.stupen+1@imbuesystems.com",
     correctPassword = "IlyaStupen_99",
+    urlLoginIntoSite = "https://dev.toothapps.com/Site/Home/Calendar",
 }
