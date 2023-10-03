24d80f63ffae085a44eb33d7d1a24e0e23201272
Aleksander Avtushko <aleksandravtusko_gmail_com>
2023-10-03 22:57:10 +0300

    refactor: change enum name

--- cypress/fixtures/common.ts
+++ cypress/fixtures/common.ts
@@ -1,7 +1,8 @@
-export enum LoginData {
+export enum CommonData {
     incorectEmail = "ilya.stupenWrongEmail@imbuesystems.com",
     incorectPassword = "incorectPassword!@#$%123",
     correctEmail = "ilya.stupen+1@imbuesystems.com",
     correctPassword = "IlyaStupen_99",
     urlLoginIntoSite = "https://dev.toothapps.com/Site/Home/Calendar",
+    urlMarketingPage = "https://dev.toothapps.com/Marketing/Home",
 }
