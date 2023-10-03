fa426c36debac869d8f9b76f30fbe9ffa1cd2bd2
Aleksander Avtushko <aleksandravtusko_gmail_com>
2023-10-03 22:58:04 +0300

    refactor: change enum name

--- cypress/e2e/RegressionTestsTS/LogInFunction.cy.ts
+++ cypress/e2e/RegressionTestsTS/LogInFunction.cy.ts
@@ -1,4 +1,4 @@
-import { LoginData } from "../../fixtures/common";
+import { CommonData } from "../../fixtures/common";
 
 describe("Test log in functions", () => {
     beforeEach(() => {
@@ -9,10 +9,10 @@ describe("Test log in functions", () => {
 
     it("check: Sign in pop-up should be opened after click on LogIn button", () => {
         cy.get('[class="sc-bRBYWo eOipuu"]').click();
-        cy.get('[class="MuiDialogContent-root dialog-content sc-gldTML gzoRBC"]').should("be.visible");
+        cy.get('[class="MuiDialogContent-root dialog-content sc-gldTML gzoRBC"]').should("exist");
     });
 
-    it.only("check: Sign in pop-up should be closed after click on close button", () => {
+    it("check: Sign in pop-up should be closed after click on close button", () => {
         cy.get('[class="sc-bRBYWo eOipuu"]').click();
         cy.get('[class="MuiDialogContent-root dialog-content sc-gldTML gzoRBC"]').should("be.visible");
         cy.get('[class="MuiButtonBase-root MuiIconButton-root sc-iIHSe jRuRaZ close-button"]').click();
@@ -22,14 +22,14 @@ describe("Test log in functions", () => {
     it("check: incorrect password data", () => {
         cy.get('[class="sc-bRBYWo eOipuu"]').click();
         cy.get('[class="sc-VigVT sc-bmyXtO haGtID"]').type("ilya.stupen+1@imbuesystems.com");
-        cy.get('[class="sc-VigVT sc-bmyXtO duXQWC"]').type(LoginData.incorectPassword);
+        cy.get('[class="sc-VigVT sc-bmyXtO duXQWC"]').type(CommonData.incorectPassword);
         cy.get('[class="sc-LKuAh sc-iBEsjs frrQCc"]').click();
         cy.get('[class="sc-cpmKsF dFrijd message"]').should("be.visible");
     });
 
     it("check: incorrect email data", () => {
         cy.get('[class="sc-bRBYWo eOipuu"]').click();
-        cy.get('[class="sc-VigVT sc-bmyXtO haGtID"]').type(LoginData.incorectEmail);
+        cy.get('[class="sc-VigVT sc-bmyXtO haGtID"]').type(CommonData.incorectEmail);
         cy.get('[class="sc-VigVT sc-bmyXtO duXQWC"]').type("IlyaStupen_99");
         cy.get('[class="sc-LKuAh sc-iBEsjs frrQCc"]').click();
         cy.get('[class="sc-cpmKsF dFrijd message"]').should("be.visible");
@@ -37,9 +37,9 @@ describe("Test log in functions", () => {
 
     it("check: logIn with correct email and password data", () => {
         cy.get('[class="sc-bRBYWo eOipuu"]').click().wait(5000);
-        cy.get('[class="sc-VigVT sc-bmyXtO haGtID"]').type(LoginData.correctEmail);
-        cy.get('[class="sc-VigVT sc-bmyXtO duXQWC"]').type(LoginData.correctPassword);
+        cy.get('[class="sc-VigVT sc-bmyXtO haGtID"]').type(CommonData.correctEmail);
+        cy.get('[class="sc-VigVT sc-bmyXtO duXQWC"]').type(CommonData.correctPassword);
         cy.get('[class="sc-LKuAh sc-iBEsjs frrQCc"]').click();
-        cy.url({ timeout: 7000 }).should("equal", LoginData.urlLoginIntoSite);
+        cy.url({ timeout: 7000 }).should("equal", CommonData.urlLoginIntoSite);
     });
 });
